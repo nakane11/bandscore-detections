2ccd6ee4ac32bd506f5b4e00e9075b65dd05d30d
yuki-snown <yuki.snowman.0103@icloud.com>
2020-08-19 22:39:41 +0900

    修正(1)

---
+++ yuki-edit.md
@@ -0,0 +1,17 @@
+## VScodeの便利tipsを知っておいた方が良い
+ - ### 拡張機能やらdebug機能
+
+## .DS_Storeはリポジトリに入れない方がいい
+ - ### .gitignoreを書いた方がいい
+    - #### https://qiita.com/anqooqie/items/110957797b3d5280c44f
+
+## package listを入れたほうが良い
+ - ### requirements.txtを作る
+    - #### https://qiita.com/sakusaku12/items/21083c73c8afa4f6c78d
+
+## 現状では入力がPOST/methodのファイルパラメータで送られ、出力がredirectで送られている。
+ - ### APIの理解とcorsの理解があった方が良い。jsonifyなども知識も
+    - #### https://qiita.com/NagaokaKenichi/items/0647c30ef596cedf4bf2
+    - #### https://qiita.com/att55/items/2154a8aad8bf1409db2b
+    - #### https://qiita.com/ekzemplaro/items/8c3e0cfd7d3da6e407e8
+
---
+++ yuki-edit.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory, flash
+import os
+from pathlib import Path
+from pdf2image import convert_from_path, convert_from_bytes
+from PIL import Image
+import numpy as np
+import io 
+import base64
+
+app = Flask(__name__)
+
+def get_concat_v(im1, im2):
+    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+
+@app.route('/', methods=['GET', 'POST'])
+def uploads_file():
+  if request.method == 'POST':
+    if 'file' not in request.files:
+      #ファイルがなかった場合
+      flash("メッセージ")
+      return redirect(request.url)
+    
+    # poppler/binを環境変数PATHに追加する
+    poppler_dir = Path(__file__).parent.absolute() / "poppler/bin"
+    os.environ["PATH"] += os.pathsep + str(poppler_dir)
+
+    images = convert_from_bytes(request.files['file'].read())
+    
+    width=images[0].width #横幅を取得
+
+    for i, image in enumerate(images):
+      if i==0:
+        im1=image.crop((0, 75, width, 250))
+      else:
+        im2 = image.crop((0, 75, width, 250))
+        im1=get_concat_v(im1, im2)
+
+    #バイナリーに変換
+    output = io.BytesIO()
+    im1.save(output, format='PNG')
+    image_png = output.getvalue()
+    #base64に変換
+    data=base64.b64encode(image_png)
+    
+    return redirect(url_for('uploaded_file', filename=data))
+
+  return '<html><body><form method = post enctype = multipart/form-data><p><input type=file name = file><input type = submit value = Upload></form></body></html>'
+  #return render_template("mainpage.html")
+
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+
+  return '<html><body>あああ</body></html>'
+  #return render_template("name.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
