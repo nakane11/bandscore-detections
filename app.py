50fe2e764bc01cb53e28571a5c2c8bc1ddcaba20
Aoi <nakane-ao@g.ecc.u-tokyo.ac.jp>
2020-08-21 09:35:54 +0900

    yuki-editを反映。エラーになったので57行目のURLを変更

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory, flash
-from werkzeug import secure_filename
+from flask_cors import CORS
 import os
-from pdf2image import convert_from_path
+from pdf2image import convert_from_bytes
 from pathlib import Path
 from PIL import Image
 import numpy as np
@@ -9,6 +9,13 @@ import io
 import base64
 
 app = Flask(__name__)
+CORS(app)
+
+def get_concat_v(im1, im2):
+    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
 
 @app.route('/', methods=['GET', 'POST'])
 def uploads_file():
@@ -17,37 +24,14 @@ def uploads_file():
       #ファイルがなかった場合
       flash("メッセージ")
       return redirect(request.url)
-    
-    file = request.files['file']
-    filename = secure_filename(file.filename)
-
-    if filename == '':
-      #ファイル名がなかった場合
-      flash("メッセージ")
-      return redirect(request.url)
-
-    #ファイルを保存
-    file.save('./uploads/'+ filename)
 
     # poppler/binを環境変数PATHに追加する
     poppler_dir = Path(__file__).parent.absolute() / "poppler/bin"
     os.environ["PATH"] += os.pathsep + str(poppler_dir)
 
-    # PDF -> Image に変換（250dpi）
-    images = convert_from_path('./uploads/'+ filename, 250)
-    os.remove('./uploads/'+ filename)
-    
-    #画像を結合する関数
-    def get_concat_v(im1, im2):
-      dst = Image.new('RGB', (im1.width, im1.height + im2.height))
-      dst.paste(im1, (0, 0))
-      dst.paste(im2, (0, im1.height))
-      return dst
+    images = convert_from_bytes(request.files['file'].read())
 
     width=images[0].width #横幅を取得
-
-    #各パートでループする?
-    index=0
 
     for i, image in enumerate(images):
       if i==0:
@@ -61,18 +45,17 @@ def uploads_file():
     im1.save(output, format='PNG')
     image_png = output.getvalue()
     #base64に変換
-    data=base64.b64encode(image_png)
-    print(data)
+    data = base64.b64encode(image_png)
     
-    return redirect(url_for('uploaded_file', filename=filename))
+    return redirect(url_for('uploaded_file'))
 
   return '<html><body><form method = post enctype = multipart/form-data><p><input type=file name = file><input type = submit value = Upload></form></body></html>'
   #return render_template("mainpage.html")
 
 
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
+@app.route('/uploaded/')
+def uploaded_file():
 
   return '<html><body>あああ</body></html>'
   #return render_template("name.html")
